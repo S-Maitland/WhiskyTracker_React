3ad00e181100e480cfa5ec6719850e20bed9fd47
S-Maitland <scott@maitland.me.uk>
2019-06-19 11:12:24 +0100

    folder setup

---
+++ whisky_tracker/src/containers/MainController.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRoute as Router, Route, Switch} from 'react-router-dom';
+import WhiskyController from './WhiskyController';
+
+const MainController = () => {
+
+  return(
+    <div>
+      <Router>
+        <React.Fragment>
+          <NavBar>
+            <Switch>
+              <Route path="/whiskys" component="{WhiskyController}"/>
+            </Switch>
+          </NavBar>
+        </React.Fragment>
+      </Router>
+    </div>
+  )
+}
+
+export default MainController;
